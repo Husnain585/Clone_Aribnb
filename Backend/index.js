2e3e85eb742f232626bb5565947a0534b0413634
husnain585 <husnain7t9@gmail.com>
2025-08-23 05:57:41 -0700

    Mongodb connection established

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const Mongoose = require('mongoose');
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+const Mongoose_URL = "mongodb://localhost:27017/airbnb";
+
+async function main() {
+    await Mongoose.connect(Mongoose_URL);
+}
+main()
+    .then(() => {
+        console.log("Database connection established");
+    })
+    .catch(err => {
+        console.error("Database connection error:", err);
+    });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
