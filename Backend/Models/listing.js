bd7c05ad2a6ad188573b7e25cf8e015ddcb57561
husnain585 <husnain7t9@gmail.com>
2025-08-23 05:59:06 -0700

    Listing schema develop and config at index.js

---
+++ Backend/Models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type : String,
+        default : "https://unsplash.com/photos/shadows-of-two-people-making-a-heart-y1mOuOfwitg",
+        set: (v) => v === "" ? "https://unsplash.com/photos/shadows-of-two-people-making-a-heart-y1mOuOfwitg" : v
+    },
+    price: Number,
+    location: String,
+    country : String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
