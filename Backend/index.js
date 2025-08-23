677eb96986d334962046b1d2068ceaafa64172de
husnain585 <husnain7t9@gmail.com>
2025-08-23 06:54:08 -0700

    Index.ejs create to view the listings as main html/ejs file

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const Mongoose = require('mongoose');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const { render } = require('ejs');
 
 const app = express();
 
@@ -18,18 +21,36 @@ main()
         console.error("Database connection error:", err);
     });
 
-app.get("/testlisting", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "My home",
-    description: "Finest property",
-    price: 202382,
-    location: "kasur",
-    country: "pakistan",
-  });
-  await sampleListing.save();
-  console.log("sample was save");
-  res.send("successfull")
+// Middlewares
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsMate);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Index Route
+app.get("/listings", async (req, res) => {
+    try {
+        const allListings = await Listing.find({});
+        res.render("listings/index", { allListings }); 
+    } catch (err) {
+        console.error("Error fetching listings:", err);
+        res.status(500).send("Internal Server Error");
+    }
 });
+
+// app.get("/testlisting", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My home",
+//     description: "Finest property",
+//     price: 202382,
+//     location: "kasur",
+//     country: "pakistan",
+//   });
+//   await sampleListing.save();
+//   console.log("sample was save");
+//   res.send("successfull")
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
